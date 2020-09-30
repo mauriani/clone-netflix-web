d2d1e357fb93c4a5a68bc00fbc38dce4dd175c8e
mauriani <maurianim@gmail.com>
2020-09-30 12:21:59 -0300

    atualizações

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import MovieRow from "./components/MovieRow";
 import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
 import Tmdb from "./Tmdb";
 import "./App.css";
 
@@ -32,8 +33,8 @@ export default () => {
 
   return (
     <div className="page">
+      <Header />
       {featuredData && <FeaturedMovie item={featuredData} />}
-
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
