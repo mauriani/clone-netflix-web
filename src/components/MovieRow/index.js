800d5d37396f3b9442528ddc4713f9be4232f110
mauriani <maurianim@gmail.com>
2020-09-30 15:57:13 -0300

    scroll funcionando

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NavigateNextIcon from "@material-ui/icons/NavigateNext";
 import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
@@ -6,20 +6,43 @@ import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
 import "./styles.css";
 
 export default ({ title, items }) => {
+  const [scrollX, setScrollX] = useState(-400);
+  const handleLeftArrow = () => {
+    let x = scrollX + Math.round(window.innerWidth / 2);
+    if (x > 0) {
+      x = 0;
+    }
+    setScrollX(x);
+  };
+
+  const handleRightArrow = () => {
+    let x = scrollX - Math.round(window.innerWidth / 2);
+    // largura da lista
+    let listaW = items.results.length * 150;
+    // conta para saber o limite
+
+    if (window.innerWidth - listaW > x) {
+      x = window.innerWidth - listaW - 60;
+    }
+    setScrollX(x);
+  };
   return (
     <div className="movieRow">
       <h2>{title}</h2>
 
-      <div className="movieRow--left">
+      <div className="movieRow--left" onClick={handleLeftArrow}>
         <NavigateBeforeIcon style={{ fontSize: 50 }} />
       </div>
 
-      <div className="movieRow--right">
+      <div className="movieRow--right" onClick={handleRightArrow}>
         <NavigateNextIcon style={{ fontSize: 50 }} />
       </div>
 
       <div className="movieRow--listarea">
-        <div className="movieRow--list">
+        <div
+          className="movieRow--list"
+          style={{ marginLeft: scrollX, width: items.results.length * 150 }}
+        >
           {items.results.length > 0 &&
             items.results.map((item, key) => (
               <div key={key} className="movieRow--item">
